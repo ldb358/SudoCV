7cce54b82a4bb57fc70ac4764cfa7036992b7820
Lane Breneman <lane@xipiter.com>
2016-06-16 20:18:36 -0700

    finished build env

---
+++ CMakeFiles/sudocv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sudocv.dir/sudo.cpp.o"
+  "CMakeFiles/sudocv.dir/utils/cache.cpp.o"
+  "sudocv.pdb"
+  "sudocv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sudocv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
